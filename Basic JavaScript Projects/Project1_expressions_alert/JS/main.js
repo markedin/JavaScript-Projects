a54e1b76f591e1ec5e9672167498efcfd37e217d
markedin <119455170+markedin@users.noreply.github.com>
2023-01-02 13:24:31 -0800

    This is the first JS i have written in the bootcamp
    Includes various functions and comments describing what the functions do line by line

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("Hello World!");
+document.write("Hello World!");
+var Sent1 = "This is the beginning of the string ";
+var Sent2 = " and this is the end of the string";
+conVar = Sent1 + Sent2;
+document.write(conVar);
+
+
+
+
+
+
+var Family = "Edin", son = "Mark", dad = "MarkSr", mom = "Sanja", Uncle = "Ted" ; //Assigning a name to the family variable
+document.write(son); // as well as assigning names to the son, dad, mom, and uncle variable
+
+document.write(3+4); //Displaying the sum of 3+4 
