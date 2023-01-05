f22e900ae62cde693e1f37a07e280f5a94f9d935
markedin <119455170+markedin@users.noreply.github.com>
2023-01-05 10:11:34 -0800

    This push includes the project 8 folder
    This folder includes index.html and main.js to satisfy the project 8 requirements

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,37 @@
+function sliceMethod() { //This line defines a function called sliceMethod
+    Sentence = "All work and no play makes Johnny a dull boy.";     //this object defines a string "Sentence"
+    Section = Sentence.slice(27,33);    //this line slices the string to display the char positions within the parameters inside the .slice() method
+    document.getElementById("Slice").innerHTML = Section;   //this line sets the HTML element "Slice" to the sliced string
+}
+
+function upperCase(){   //this line defines a function called upperCase
+    string = "hello how are you?";  //this line defines a string callled "string"
+    upStr = string.toUpperCase();   // this line converts the string to upper case and stores it in upStr
+    document.write(upStr);  //this line writes the upStr to the doc when function is called 
+}
+
+function searchFunction(){  // this line defines a function called searchFunction
+    string = "A B C D E F G";   //this line defines a string 
+    searchStr = string.search("D");     // this line searches for the position of "D" within the string "string"
+    console.log(searchStr); //this line writes the position of "D" within the browser console
+}
+
+function num2str(){ // this line defines a function called num2str
+    x = 68419;  // this line defines a number
+    document.getElementById("Number2String").innerHTML = x.toString(); // this line converts the number var x to a string 
+}
+
+function precisionMethod(){     //this line defines a function called precisionMethod
+    x = 123.456789; //This line defines a number called x
+    document.getElementById("Precision").innerHTML = x.toPrecision(4); // this line converts number x to a number with 4 digits total
+}
+
+function toFixedMethod(){   //this line defines a function called toFixedMethod
+    x = 123.456789; //this line defines a number x
+    document.getElementById("toFixed").innerHTML = x.toFixed(5); // // this line converts number x to a floating point number with 5 significant figures
+}
+
+function obStr2str(){ // this line defines a function called obStr2str
+    str = "Hello World! I am an string";    // this line defines a string
+    document.getElementById("obStr").innerHTML = str.valueOf();     //this line converts a string object to a string without changing the string itself
+}
